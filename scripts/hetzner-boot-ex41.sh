f995f14038cc43ffa626b9bb715e9a60837cca4f
niclas <niclas@hedhman.org>
2019-04-03 13:50:43 +0800

    ...

--- scripts/hetzner-boot-ex41.sh
+++ scripts/hetzner-boot-ex41.sh
@@ -12,14 +12,17 @@ if [ -z $1 ] ; then
 fi
 
 HOST=$1
-FULLHOST=$1.bali.io
-IP=$(dig +search +short $HOST | grep ^[0-9])
+FULLHOST=$1.sensetif.net
+IP=$(dig +search +short $FULLHOST | grep ^[0-9])
 ssh-keygen -q -R $HOST
 ssh-keygen -q -R $FULLHOST
 ssh-keygen -q -R $IP
 ssh-keyscan -H $FULLHOST >> ~/.ssh/known_hosts
 
-ssh root@$FULLHOST "/root/.oldroot/nfs/install/installimage \
+ssh root@$FULLHOST "\
+	      dd if=/dev/zero of=/dev/sda count=10240 bs=1024 && \
+	      dd if=/dev/zero of=/dev/sdb count=10240 bs=1024 && \
+	      /root/.oldroot/nfs/install/installimage \
               -a  \
               -b grub \
               -r no \
@@ -27,10 +30,10 @@ ssh root@$FULLHOST "/root/.oldroot/nfs/install/installimage \
               -d sda \
               -n $HOST \
               -p /boot:ext3:1G,lvm:vg0:all \
-              -v vg0:root:/:ext4:100G,vg0:swap:swap:swap:32G,vg0:home:/home:xfs:100G,vg0:var:/var:xfs:200G,vg0:ipfs:/ipfs:xfs:2000G \
+              -v vg0:root:/:ext4:100G,vg0:swap:swap:swap:32G,vg0:home:/home:xfs:100G,vg0:var:/var:xfs:1000G,vg0:ipfs:/ipfs:xfs:500G \
               -s en \
               -i /root/images/Debian-94-stretch-64-minimal.tar.gz \
-              -K https://raw.githubusercontent.com/eosgermany/public_meta/master/authorized_keys \
+              -K https://raw.githubusercontent.com/Sensetif/public_data/master/authorized_keys \
               && rm /etc/ssh/ssh_host_rsa_key \
               && rm /etc/ssh/ssh_host_dsa_key \
               && rm /etc/ssh/ssh_host_ecdsa_key \
